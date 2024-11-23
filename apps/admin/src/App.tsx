2e7e42ce4dc0ae736ccd0fda84140d9996e72abd
Eduardo Alvarez <el_alienx@hotmail.com>
2024-11-23 16:03:16 +0100

    added a local component to separate from the shared components

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,9 +1,13 @@
 // Shared
 import { Button } from "@monorepo-plus/components";
 
+// Project files
+import NavigationBar from "./components/navigation-bar/NavigationBar";
+
 export default function App() {
   return (
     <div className="app">
+      <NavigationBar />
       <h1>👩‍💼 Admin</h1>
       <Button onClick={() => window.open("https://admin.lendo.se", "_blank")} label="Open" />
     </div>
---
+++ apps/admin/src/components/navigation-bar/NavigationBar.tsx
@@ -0,0 +1,15 @@
+// Project files
+import "./navigation-bar.css";
+
+export default function NavigationBar() {
+  return (
+    <nav className="navigation-bar">
+      <h2>Admin sidebar</h2>
+      <ul>
+        <li>Business Application</li>
+        <li>Mortage Application</li>
+        <li>Private Application</li>
+      </ul>
+    </nav>
+  );
+}
