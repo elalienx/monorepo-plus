5f457bf861916bcc9b0c4ac062f86939525f83ba
Eduardo Alvarez <el_alienx@hotmail.com>
2024-11-23 15:53:37 +0100

    added library for components

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,14 +1,11 @@
 // Shared
-// import Button from "@monorepo-plus/shared/components/src/lib/button/Button"; // Ensure correct import path
-
-import { Button, Components } from "@monorepo-plus/components";
+import { Button } from "@monorepo-plus/components";
 
 export default function App() {
   return (
     <div className="app">
       <h1>👩‍💼 Admin</h1>
-      <Components />
-      <Button onClick={() => window.open("https://admin.lendo.se", "_blank")} label="View" />
+      <Button onClick={() => window.open("https://admin.lendo.se", "_blank")} label="Open" />
     </div>
   );
 }
--- apps/inbox/src/App.tsx
+++ apps/inbox/src/App.tsx
@@ -1,8 +1,11 @@
+// Shared
+import { Button } from "@monorepo-plus/components";
+
 export default function App() {
   return (
     <div className="app">
       <h1>📥 Inbox</h1>
-      <button onClick={() => window.open("https://inbox.lendo.se", "_blank")}>View</button>
+      <Button onClick={() => window.open("https://inbox.lendo.se", "_blank")} label="Open" />
     </div>
   );
 }
