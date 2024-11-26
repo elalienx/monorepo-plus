986415da935fad8a752d62be35fa811d4f3832b5
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2024-11-26 16:41:20 +0100

    🐞 fix: fixed hardcoded word "admin" introduced when updating the playwright configurations

--- e2e/inbox/playwright.config.ts
+++ e2e/inbox/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npx nx run admin:preview",
+    command: "npx nx run inbox:preview",
     url: "http://localhost:4300",
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
