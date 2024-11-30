303b837aa173906f9e1e99700a7d6ce98ccb9bef
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2024-11-30 13:02:09 +0100

    added specific version of Biome

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,10 +2,10 @@
 import { Button } from "@monorepo-plus/aphrodite";
 
 export default function App() {
-	return (
-		<div className="app">
-			<h1>👩‍💼 Admin 2</h1>
-			<Button label="Click me" onClick={() => alert("Admin page")} />
-		</div>
-	);
+  return (
+    <div className="app">
+      <h1>👩‍💼 Admin 2</h1>
+      <Button label="Click me" onClick={() => alert("Admin page")} />
+    </div>
+  );
 }
