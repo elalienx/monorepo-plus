352976cb9e4215eae0142ba9528c7d568751c6cb
Eduardo Alvarez <el_alienx@hotmail.com>
2024-11-30 21:46:39 +0100

    added cosmos config for admin

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -12,10 +12,6 @@ export default defineConfig({
     port: 4200,
     host: "localhost",
   },
-  preview: {
-    port: 4300,
-    host: "localhost",
-  },
   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
   build: {
     outDir: "../../dist/apps/admin",
