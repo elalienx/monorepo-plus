651276a4699e994ba8922cf0c22ee126f4230ae0
Eduardo Alvarez <el_alienx@hotmail.com>
2024-11-30 23:18:49 +0100

    added test on Admin to verify if the Playwright cache is installd

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <div className="app">
       <h1>👩‍💼 Admin</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio delectus sequi quasi assumenda facere dolor earum
+        ab, distinctio soluta suscipit reprehenderit, natus maxime in ut maiores nemo beatae neque fuga?
+      </p>
       <Button label="Click me" onClick={() => alert("Admin page")} />
     </div>
   );
