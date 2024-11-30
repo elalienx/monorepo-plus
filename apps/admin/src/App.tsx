55316899b4c93255cff53e8ead8486b66d6de4f4
Eduardo Alvarez <el_alienx@hotmail.com>
2024-12-01 00:15:36 +0100

    failing app test on purpose to see if PW is cached but monitoring changes

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "@monorepo-plus/aphrodite";
 export default function App() {
   return (
     <div className="app">
-      <h1>👩‍💼 Admin</h1>
+      <h1>👩‍💼 Manager</h1>
       <Button label="Click me" onClick={() => alert("Admin page")} />
     </div>
   );
