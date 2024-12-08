ce40a2ee69acb2325283fb2dfe84e3e63f8ee522
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2024-12-08 15:39:02 +0100

    mived e2e folder

--- e2e/inbox/playwright.config.ts
+++ e2e/inbox/playwright.config.ts
@@ -1,20 +1,13 @@
 import { defineConfig, devices } from "@playwright/test";
 import { nxE2EPreset } from "@nx/playwright/preset";
 
+import { fileURLToPath } from "url";
 import { workspaceRoot } from "@nx/devkit";
 
-// For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env["BASE_URL"] || "http://localhost:3000";
+const baseURL = process.env["BASE_URL"] || "http://localhost:4300";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const __filename = fileURLToPath(import.meta.url);
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: "./src" }),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -25,8 +18,8 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm exec nx run inbox:serve-static",
-    url: "http://localhost:3000",
+    command: "npx nx run inbox:preview",
+    url: "http://localhost:4300",
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
   },
