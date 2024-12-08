6eebb08484b35efe2364debf8d45233b3cda367c
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2024-12-08 16:21:52 +0100

    chore: added biome config to inbox

--- apps/inbox/src/app/page.tsx
+++ apps/inbox/src/app/page.tsx
@@ -12,8 +12,9 @@ export default function Index() {
       <h1>📥 Inbox</h1>
       <Image src={Illustration} alt="" />
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi id accusantium illum natus, iure nam sint
-        vitae error iusto alias nihil totam quisquam omnis ipsum! Mollitia est porro laudantium error!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi id
+        accusantium illum natus, iure nam sint vitae error iusto alias nihil
+        totam quisquam omnis ipsum! Mollitia est porro laudantium error!
       </p>
       <Button label="Click me" onClick={() => alert("Admin page")} />
     </div>
