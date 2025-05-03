0afc02df3a5f7c683823fd85b3d57ff7f0736e93
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-05-03 12:40:42 +0200

    refactor: simplifed path of packages and products

--- apps/inbox/src/app/page.tsx
+++ apps/inbox/src/app/page.tsx
@@ -3,7 +3,7 @@
 // Node modules
 import Image from "next/image";
 
-import { Button } from "@monorepo-plus/aphrodite";
+import { Button } from "@aphrodite";
 import Illustration from "./assets/credit-card.png";
 
 export default function Index() {
@@ -12,9 +12,8 @@ export default function Index() {
       <h1>📥 Inbox</h1>
       <Image src={Illustration} alt="" />
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi id
-        accusantium illum natus, iure nam sint vitae error iusto alias nihil
-        totam quisquam omnis ipsum! Mollitia est porro laudantium error!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi id accusantium illum natus, iure nam sint
+        vitae error iusto alias nihil totam quisquam omnis ipsum! Mollitia est porro laudantium error!
       </p>
       <Button label="Click me" onClick={() => alert("Admin page")} />
     </div>
