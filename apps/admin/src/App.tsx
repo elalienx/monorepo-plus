3abd76bb5118c50a8f091be0034ce7d6f160c145
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-05-03 12:40:42 +0200

    refactor: simplifed path of packages and products

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,15 +1,14 @@
 // Shared
-import { Button } from "@monorepo-plus/aphrodite";
+import { Button } from "@aphrodite";
 
 export default function App() {
   return (
     <div className="app">
       <h1>👩‍💼 Admin</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-        facilis molestias quod? Suscipit nemo necessitatibus eius, libero cum
-        sapiente. Consequuntur sed unde, quasi laboriosam alias sunt odio minima
-        velit mollitia!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt facilis molestias quod? Suscipit nemo
+        necessitatibus eius, libero cum sapiente. Consequuntur sed unde, quasi laboriosam alias sunt odio minima velit
+        mollitia!
       </p>
       <Button label="Click me" onClick={() => alert("Admin page")} />
     </div>
